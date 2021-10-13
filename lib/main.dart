4b194e857b87f80e7aa1c91cec16de9a5b78e634
Narasimman <mr.sfoundation@gmail.com>
2021-10-13 15:13:21 +0530

    Dark-Theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,9 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
       home: headerview(),
     );
   }
